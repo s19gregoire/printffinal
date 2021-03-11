1c72146b2f732151e9e41bab00927b2fe1894a34
y3zh <mil3zh@protonmail.com>
2021-03-11 16:50:54 +0100

    width for neg int

--- srcs/ft_output_int.c
+++ srcs/ft_output_int.c
@@ -24,12 +24,12 @@ int ft_output_int(t_print *mytab, const char *format, int pos)
 	j = va_arg(mytab->args, int);
 	(void)format;
 	num = ft_itoa(j);
-	len = ft_len(j);
-	if (mytab->width > len && mytab->dash == 0)
+	len = ft_strlen(num);
+	if (mytab->width > mytab->precision && mytab->width > len && mytab->dash == 0)
 		ft_fill_width_right(mytab,len);
 	while(num[i])
 		write(1, &num[i++], 1);
-	if (mytab->width > len && mytab->dash)
+	if (mytab->width > mytab->precision && mytab->width > len && mytab->dash)
 		ft_fill_width_left(mytab,len);
 	else
 		mytab->total_length += len;
